c3271ea13a0d5e70dab33f3c72c5be71be599743
JesseNovice <jesseroylees@gmail.com>
2025-03-22 13:16:34 +1300

    dependencies added

--- test/SendToTaxWalletTest.t.sol
+++ test/SendToTaxWalletTest.t.sol
@@ -9,11 +9,11 @@ import {Script} from "forge-std/Script.sol";
 import {Test, console} from "forge-std/Test.sol";
 
 contract SendToTaxWalletTest is Test {
-    mockerc public mockerc;
+    MockERC20 public mockerc;
     uint256 constant STARTING_BALANCE = 20 ether;
     address constant OWNER =
         address(0x4Ad50Cf71B08C7c8907A8965c84f2c517573573E);
-    address taxWallet = new taxWallet();
+    taxWallet public wallet;
 
     function setUp() external {
         mockerc = new MockERC20("MockNZDD", "mNZDD");
@@ -22,13 +22,14 @@ contract SendToTaxWalletTest is Test {
         vm.startPrank(OWNER);
         mockerc.approve(address(this), 100000);
         vm.stopPrank();
+        wallet = new taxWallet();
     }
 
     function testSendToTaxWallet() public {
         uint256 amount = 1000;
         vm.prank(OWNER);
-        taxWallet.depositNZDD(address(mockerc), amount);
+        wallet.depositNZDD(address(mockerc), amount);
         vm.stopPrank();
-        assertEq(mockerc.balanceOf(address(taxWallet)), amount);
+        assertEq(mockerc.balanceOf(address(wallet)), amount);
     }
 }
